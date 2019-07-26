7152153431b5e9f853c82c2d28b399125799cc60
shambhavibr <53291087+shambhavibr@users.noreply.github.com>
2019-07-26 22:45:34 +0530

    Update add_n_numbers.c

--- add_n_numbers.c
+++ add_n_numbers.c
@@ -1,2 +1,45 @@
-void input(int , int *)
+#include <stdio.h>
+void input(int *, int *)
 int compute(int, int*)
+void output(int)
+  
+main()
+{
+  int a[10],size,sum;
+  
+  input(&size,a);
+  sum=compute(size,a);
+  output(sum);
+}
+
+void input(int *n, int *a)
+{
+  int i;
+  printf("Enter the number of integers to be added");
+  scanf("%d",n);
+  printf("Enter the elements");
+         
+  for(i=0;i<n;i++)
+  {
+     scanf("%d",&a[i]);
+  }
+}
+
+int compute(int n, int *a)
+{
+  int i,sum=0;
+  for(i=0;i<n;i++)
+  {
+    sum=sum+a[i];
+  }
+  return sum;
+}
+
+void output(int x)
+{
+  printf("The sum is %d",x);
+}
+
+           
+         
+   
