d797e7640896feb0cc575ce6686b930f1f375e81
shambhavibr <53291087+shambhavibr@users.noreply.github.com>
2019-07-26 22:24:24 +0530

    Update Add_Two_numbers.c

--- Add_Two_numbers.c
+++ Add_Two_numbers.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 
-void input(int *x, int *y);
+void input(int *, int *);
 
-int compute(int x, int y);
+int compute(int, int);
 
-void output(int x);
+void output(int);
+
+main()
+{
+  int num1,num2;
+  input(&num1,&num2);
+  num2=compute(num1,num2);
+  output(num2); 
+}
+
+void input(int *x,int *y)
+{
+  printf("Enter first number");
+  scanf("%d",&x);
+  
+  printf("Enter second number");
+  scanf("%d",&y);
+}
+
+int compute(int x, int y)
+{
+  return x+y;
+}
+
+void output(int x)
+{
+  printf("Sum of the two numbers is %d",x);
+}
+  
+
 
 
 
